9adf854178b9f519d3bee8fae4511a69865f5646
xenobiont <61256244+xenobiont@users.noreply.github.com>
2024-01-28 05:52:13 +0200

    Initial commit

---
+++ task/task.js
@@ -0,0 +1,21 @@
+const books = [
+	{
+		author: 'J.R.R.Tolkien',
+		name: 'Lord of the Rings',
+		article: '4342D',
+		price: 120,
+	},
+	{
+		author: 'Stephen King',
+		name: 'The Dark Tower',
+		article: '4421F',
+		price: 150,
+	},
+	{
+		author: 'William Golding',
+		name: 'Lord of the Flies',
+		article: '4642D',
+		price: 100,
+	},
+];
+// Your code here
---
+++ task/task.md
@@ -0,0 +1,5 @@
+# Task
+
+Write getBookShortInfo function, that receives a book object as an argument (with 4 properties: author, name, article, price)
+and returns a new object with only 2 properties: author and name.
+Use this function to convert books array to shortBooksInfo array
